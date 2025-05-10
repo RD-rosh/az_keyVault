d158e263439613b1155498e4b54d7365555230fc
RD <134850116+RD-rosh@users.noreply.github.com>
2025-05-11 01:28:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,17 +4,14 @@ const express = require("express");
 const app = express();
 
 const keyVaultName = "RoshKeyvault"; // your vault name
-//const vaultUrl = `https://${keyVaultName}.vault.azure.net`;
-const vaultUrl = 'https://roshkeyvault.vault.azure.net';
+const vaultUrl = `https://${keyVaultName}.vault.azure.net`;
 const credential = new DefaultAzureCredential();
 const client = new SecretClient(vaultUrl, credential);
 
 app.get("/", async (req, res) => {
-  
   try {
     const secret = await client.getSecret("ROSHsecretKey"); //you secret name
-    res.send('Hello from Azure App Service!');
-    res.send(`Secret value: ${secret.value}`);
+    res.send(`Hello from Azure App Service! Your Secret value: ${secret.value}`);
   } catch (err) {
     console.error(err);
     res.status(500).send("Error fetching secret");
