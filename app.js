2c0eb1a2c09e870371428d6985e2ba5cbef70317
RD <134850116+RD-rosh@users.noreply.github.com>
2025-05-10 21:45:16 +0530

    update_file

---
+++ app.js
@@ -0,0 +1,21 @@
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+const express = require("express");
+const app = express();
+
+const keyVaultName = "RoshKeyvault"; // your vault name
+const vaultUrl = `https://${keyVaultName}.vault.azure.net`;
+const credential = new DefaultAzureCredential();
+const client = new SecretClient(vaultUrl, credential);
+
+app.get("/", async (req, res) => {
+  try {
+    const secret = await client.getSecret("ROSHsecretKey"); //you secret name
+    res.send(`Secret value: ${secret.value}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error fetching secret");
+  }
+});
+
+app.listen(3000, () => console.log("App running on http://localhost:3000"));
